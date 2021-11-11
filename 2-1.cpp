2c1672d5d8e6aa65c7e479786ad30c89642498ed
Шишконаков Дмитрий Алексеевич <shishkonakov@bk.ru>
2021-11-11 22:00:10 +0300

    Добавлено решение 2-1.cpp

---
+++ 2-1.cpp
@@ -0,0 +1,92 @@
+﻿#include <iostream>
+using namespace std;
+/*
+* \brief увеличение числа a в n раз
+* \param a число a
+* \param n число n
+* \return число a увеличенное в n раз
+*/
+int getproduct(int a, int n);
+
+/*
+* \brief увеличение числа a в 2n раз
+* \param a число a
+* \param n число n
+* \return число a увеличенное в 2n раз
+*/
+int getdoubleproduct(int a, int n);
+
+/*
+* \brief увеличение числа a в 3n раз
+* \param a число a
+* \param n число n
+* \return число a увеличенное в 3n раз
+*/
+int gettripleproduct(int a, int n);
+
+/*
+* \brief выбор заполнения массива
+*/
+enum class path
+{
+	Single = 1,
+	Double = 2,
+	Triple = 3
+};
+
+/*
+* \brief Точка входа в программу.
+* \return Код ошибки, если 0 - успешное выполнение.
+*/
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	cout << "Введите число A : ";
+	int a;
+	cin >> a;
+	cout << "Введите число N : ";
+	int n;
+	cin >> n;
+	cout << "Выберите формулу:\n1: A * N\n2: 2 * A * N\n3: 3 * A * N\nВыбор: ";
+	int choice;
+	cin >> choice;
+	const auto filling = static_cast<path>(choice);
+	switch (filling) {
+	case path::Single:
+		{
+			cout << "A * N = " << getproduct(a, n);
+			break;
+		}
+	case path::Double:
+		{
+			cout << "2 * A * N = " << getdoubleproduct(a, n);
+			break;
+		}
+	case path::Triple:
+		{
+			cout << "3 * A * N = " << gettripleproduct(a, n);
+			break;
+		}
+	default:
+	{
+		cout << "Некоректный ввод.";
+		return (0);
+	}
+	}
+	return 0;
+}
+
+int getproduct(int a, int n)
+{
+	return (a * n);
+}
+
+int getdoubleproduct(int a, int n)
+{
+	return (a * 2 * n);
+}
+
+int gettripleproduct(int a, int n)
+{
+	return (a * 3 * n);
+}
