791ebf2230dc1d2569d95444f20f48db76c7e45b
Shishkonakov Dmitry/Rubirc <81958818+Rubirc@users.noreply.github.com>
2021-11-26 06:57:39 +0300

    Update 3-1.cpp

--- 3-1.cpp
+++ 3-1.cpp
@@ -11,16 +11,24 @@ using namespace std;
 **/
 double calcFunction(const double x);
 
+bool isCalculated(const double x);
+
 int main()
 {
-	const double LOWER_BOUND = 0, UPPER_BOUND = 1, STEP = 0.1 
-        const int WIDTH = 5;
+	setlocale(LC_ALL, "Russian");
+	cout << fixed;
+
+	const double LOWER_BOUND = 0, UPPER_BOUND = 1, STEP = 0.1;
+	const int WIDTH = 5;
 
 	cout << setw(WIDTH) << "X" << " | " << setw(2 * WIDTH) << "Y\n";
 
 	for (double x = LOWER_BOUND; x < UPPER_BOUND; x += STEP)
 	{
-		cout << setw(WIDTH) << x << " | " << setw(2 * WIDTH) << calcFunction(x) << "\n";
+		if (!isCalculated(x))
+			cout << setw(WIDTH) << x << " | " << setw(2 * WIDTH) << "невозможно вычислить значение функции в данной точке.\n";
+		else
+			cout << setw(WIDTH) << x << " | " << setw(2 * WIDTH) << calcFunction(x) << "\n";
 	}
 
 	return 0;
@@ -29,4 +37,9 @@ int main()
 double calcFunction(const double x)
 {
 	return sqrt(1.0 - x) - cos(sqrt(1.0 - x));
+}
+
+bool isCalculated(const double x)
+{
+	return ((1.0 - x) >= 0);
 }
