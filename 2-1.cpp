22c469c18f84707b599f3a188362173473a31982
Shishkonakov Dmitry-tki-212/Rubirc <81958818+shishkonakov-dmitry-tki212@users.noreply.github.com>
2021-11-28 21:58:38 +0300

    Update 2-1.cpp

--- 2-1.cpp
+++ 2-1.cpp
@@ -6,7 +6,7 @@ using namespace std;
 * \param n число n
 * \return число a увеличенное в n раз
 */
-const int GetProduct(int a, int n);
+int GetProduct(const int a, const int n);
 
 /*
 * \brief увеличение числа a в 2n раз
@@ -14,7 +14,7 @@ const int GetProduct(int a, int n);
 * \param n число n
 * \return число a увеличенное в 2n раз
 */
-const int GetDoubleProduct(int a, int n);
+int GetDoubleProduct(const int a, const int n);
 
 /*
 * \brief увеличение числа a в 3n раз
@@ -22,16 +22,16 @@ const int GetDoubleProduct(int a, int n);
 * \param n число n
 * \return число a увеличенное в 3n раз
 */
-const int GetTripleProduct(int a, int n);
+int GetTripleProduct(const int a, const int n);
 
 /*
 * \brief выбор уравнения
 */
 enum class path
 {
-	Single = 1,
-	Double = 2,
-	Triple = 3
+    Single = 1,
+    Double = 2,
+    Triple = 3
 };
 
 /*
@@ -40,53 +40,57 @@ enum class path
 */
 int main()
 {
-	setlocale(LC_ALL, "Russian");
-	cout << "Введите число A : ";
-	int a;
-	cin >> a;
-	cout << "Введите число N : ";
-	int n;
-	cin >> n;
-	cout << "Choose an option:\n" << "1) Увеличение в N раз\n" << "2) Увеличение в 2N раз\n" << "3) Увеличение в 3N раз\n"  << "1 or 2 or 3: ";
-	int choice;
-	cin >> choice;
-	const auto filling = static_cast<path>(choice);
-	switch (filling) {
-	case path::Single:
-	{
-		cout << "A * N = " << GetProduct(a, n);
-		break;
-	}
-	case path::Double:
-	{
-		cout << "2 * A * N = " << GetDoubleProduct(a, n);
-		break;
-	}
-	case path::Triple:
-	{
-		cout << "3 * A * N = " << GetTripleProduct(a, n);
-		break;
-	}
-	default:
-	{
-		cout << "Некоректный ввод.";
-		return (0);
-	}
-	}
-	return 0;
+    setlocale(LC_ALL, "Russian");
+    cout << "Введите число A : ";
+    int a;
+    cin >> a;
+    cout << "Введите число N : ";
+    int n;
+    cin >> n;
+    cout << "Выберете что делать дальше:\n"
+        << "Введите " << static_cast<int>(path::Single) << " - для увеличения A в N раз,\n"
+        << "Введите " << static_cast<int>(path::Double) << " - для увеличения A в 2N раз,\n"
+        << "Введите " << static_cast<int>(path::Triple) << " - для увеличения A в 3N раз,\n"
+        << "Ваш выбор: ";
+    int choice;
+    cin >> choice;
+    const auto chosen = static_cast<path>(choice);
+    switch (chosen) {
+    case path::Single:
+    {
+        cout << "A * N = " << GetProduct(a, n);
+        break;
+    }
+    case path::Double:
+    {
+        cout << "2 * A * N = " << GetDoubleProduct(a, n);
+        break;
+    }
+    case path::Triple:
+    {
+        cout << "3 * A * N = " << GetTripleProduct(a, n);
+        break;
+    }
+    default:
+    {
+        cout << "Некоректный ввод.";
+        return (0);
+    }
+    }
+    return 0;
 }
 
-const int GetProduct(int a, const int n)
+int GetProduct(const int a, const int n)
 {
-	return (a * n);
+    return (a * n);
 }
 
-const int GetDoubleProduct(int a, const int n)
+int GetDoubleProduct(const int a, const int n)
 {
-	return (a * 2 * n);
+    return (a * 2 * n);
 }
 
-const int GetTripleProduct(int a, const int n)
+int GetTripleProduct(const int a, const int n)
 {
-	return (a * 3 * n);
+    return (a * 3 * n);
 }
